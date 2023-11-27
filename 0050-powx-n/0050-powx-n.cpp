5b820e80299bbcf69381ced3b127b822ce010b03
zekua <zekudk@gmail.com>
2023-11-27 23:39:54 +0300

    Time: 3 ms (41.97%), Space: 6.5 MB (34.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long num = abs(n);
+        double result = 1.0;
+        while(num != 0){
+            if(num %2 == 1){
+                result*=x;
+                num--;
+            }
+            x*=x;
+            num/=2;
+        }
+        return n<0? 1.0/result : result;
     }
 };
