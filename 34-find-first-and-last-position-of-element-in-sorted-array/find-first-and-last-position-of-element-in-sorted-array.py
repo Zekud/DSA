e9777639265579af8b3eb147ef02b9128360a00c
Zekariyas Kumsa <zekudk@gmail.com>
2025-03-24 17:43:25 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (74.65%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        foccur = float("inf")
+        loccur = float("-inf")
+        def bsearch(target,start,end,occ,foccur,loccur):
+            while start <= end:
+                mid = start + (end-start)//2
+                if nums[mid] == target:
+                    if occ == "first":
+                        foccur = min(mid,foccur)
+                        end= mid-1
+                    else:
+                        loccur = max(mid,loccur)
+                        start = mid+1
+                elif nums[mid] > target:
+                    end = mid-1
+                else:
+                    start = mid+1
+            if occ == "first" and foccur != float("inf"):
+                return foccur
+            elif occ == "last" and loccur != float("-inf"):
+                return loccur
+            else:
+                return -1
+        first = bsearch(target,0,len(nums)-1,"first",foccur,loccur)
+        last = bsearch(target,0,len(nums)-1,"last",foccur,loccur)
+        return [first,last]
+        
+        
