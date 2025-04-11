1a44494e770ca6de55fd7ea79dbec8a9f915d054
Zekariyas Kumsa <zekudk@gmail.com>
2025-04-11 12:40:47 +0300

    Time: 180 ms (59.89%) | Memory: 32.7 MB (87.37%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # def getMiddle(head):
+        #     fast,slow=head,head
+        #     while fast.next and fast.next.next:
+        #         slow=slow.next
+        #         fast=fast.next.next
+        #     return slow
+        def mergeSort(left,right):
+            dummy=ListNode()
+            ptr=dummy
+            while left and right:
+                if left.val<right.val:
+                    ptr.next=left
+                    left=left.next
+                    ptr=ptr.next
+                else:
+                    ptr.next=right
+                    right=right.next
+                    ptr=ptr.next
+            ptr.next = left if left else right
+            return dummy.next
+        def merge(head):
+            if not head or not head.next:
+                return head
+            fast,slow=head,head
+            while fast.next and fast.next.next:
+                slow=slow.next
+                fast=fast.next.next
+            
+            right_head=slow.next
+            slow.next=None
+            left=merge(head)
+            right=merge(right_head)
+            return mergeSort(left,right)
+       
+
+        return merge(head)
+
+            
