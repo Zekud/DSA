e1a615f8ddbc2b3d7d98569ead6d14358bac9a3d
Zekariyas Kumsa <zekudk@gmail.com>
2025-05-01 16:00:55 +0300

    Time: 378 ms (71.14%) | Memory: 19.8 MB (35.47%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,19 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u,v,w in times:
+            graph[u].append((v,w))
+
+        min_heap = [(0,k)]
+        visited = {}
+        while min_heap:
+            time,node = heappop(min_heap)
+            if node in visited:
+                continue
+            visited[node] = time
+            for nei,wt in graph[node]:
+                if nei not in visited:
+                    heappush(min_heap,(time + wt,nei))
+        if len(visited) == n:
+            return max(visited.values())
+        return -1
