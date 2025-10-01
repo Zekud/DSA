da9c04eaa44666b8e5ab0a57caa3570f23ae0c06
Zekariyas Kumsa <zekudk@gmail.com>
2025-10-01 11:30:47 +0300

    Time: 71 ms (75.89%) | Memory: 60.9 MB (62.32%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,17 +1,33 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        memo = {}
-        n = len(questions)
-        def dp(i):
-            if i >= n:
-                return 0
-            if i in memo:
-                return memo[i]
-            # not take
-            res = dp(i+1)
+        # top down
+        # memo = {}
+        # n = len(questions)
+        # def dp(i):
+        #     if i >= n:
+        #         return 0
+        #     if i in memo:
+        #         return memo[i]
+        #     # not take
+        #     res = dp(i+1)
 
+        #     # take
+        #     memo[i] = max(res,questions[i][0] + dp(i+questions[i][1]+1))
+        #     return memo[i]
+        # return dp(0)
+        # bottom up
+        n = len(questions)
+        dp = [0] * (n+1) #so we can handle the base case dp[n]=0
+        for i in range(n-1,-1,-1):
+            pts = questions[i][0]
+            bp = questions[i][1]
+            # not take
+            nottake = dp[i+1]
             # take
-            memo[i] = max(res,questions[i][0] + dp(i+questions[i][1]+1))
-            return memo[i]
-        return dp(0)
-        
+            take = pts
+            if i + bp + 1 <=n:
+                take = pts + dp[i + bp + 1]
+            dp[i] = max(take,nottake)
+        return dp[0]
+
+
