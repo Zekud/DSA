240dde65530857b0f9d76a5845a28e7ab9617412
Zekariyas Kumsa <zekudk@gmail.com>
2025-10-10 19:22:08 +0300

    Time: 48 ms (26.34%) | Memory: 17.5 MB (99.86%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,17 +1,27 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        memo = {}
-        def backtrack(curr):
-            if curr == 0:
-                return 1
-            if curr <0:
-                return 0
-            if curr in memo:
-                return memo[curr]
-            memo[curr] = 0
+        # memo = {}
+        # def backtrack(curr):
+        #     if curr == 0:
+        #         return 1
+        #     if curr < 0:
+        #         return 0
+        #     if curr in memo:
+        #         return memo[curr]
+        #     memo[curr] = 0
             
-            for i in range(len(nums)):
-                memo[curr] += backtrack(curr-nums[i])
+        #     for i in range(len(nums)):
+        #         memo[curr] += backtrack(curr-nums[i])
             
-            return memo[curr]
-        return backtrack(target)
+        #     return memo[curr]
+        # return backtrack(target)
+        # bottom up
+        dp = [0]*(target+1)
+        dp[0] = 1
+        for i in range(1,len(dp)):
+            for num in nums:
+                if i - num >= 0:
+                    dp[i] += dp[i-num]
+        return dp[target]
+
+
